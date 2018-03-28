2f19c73c0e3a67a88cb6dd482821384fe088269d
MrHuangJJ <13874663520@163.com>
2018-03-28 10:08:39 +0800

    初次提交：增加日志

--- src/main/java/com/lanzan/controller/UserController.java
+++ src/main/java/com/lanzan/controller/UserController.java
@@ -2,6 +2,8 @@ package com.lanzan.controller;
 
 import com.lanzan.dao.UserMapper;
 import com.lanzan.entity.U_User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,12 @@ import org.springframework.web.servlet.ModelAndView;
 public class UserController {
     @Autowired
     private UserMapper userMapper;
+    public  Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(value = "login")
     public String login(){
         U_User u_user=userMapper.getUser(new U_User("admin","admin"));
+        logger.error("登陆异常");
         System.out.println(u_user.getUser_id());
         return "index";
     }
